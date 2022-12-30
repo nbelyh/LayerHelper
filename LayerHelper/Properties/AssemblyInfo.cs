a7b00944f3e5e72a9019399d725c621c7651fe26
Nikolay Belykh <nbelyh@gmail.com>
2022-12-30 13:07:04 +0100

    add azure build

--- LayerHelper/Properties/AssemblyInfo.cs
+++ LayerHelper/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("111.111.111")]
